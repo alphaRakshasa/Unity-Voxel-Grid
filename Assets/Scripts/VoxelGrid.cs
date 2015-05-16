8d37ee69173ad35ab5e60d1919b4457e2b1d26ba
Tsinoyboi <ajsnoface@gmail.com>
2015-05-16 04:27:37 -0700

    commit
    kept failing

---
+++ Assets/Scripts/VoxelGrid.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SelectionBase]
+public class VoxelGrid : MonoBehaviour
+{
+    [SerializeField]
+    private  int resolution;
+    [SerializeField]
+    private GameObject voxelPrefab;
+    
+    private float voxelSize;
+    private bool[] voxels;
+
+    private Material[] voxelMaterials;
+
+    public void Initialize (int resolution, float size)
+    {
+        this.resolution = resolution;
+        voxelSize = 1f / (float)resolution;
+        voxels = new bool[resolution * resolution];
+        voxelMaterials = new Material[voxels.Length];
+        
+        for (int i = 0, y = 0; y < resolution; y++)
+        {
+            for (int x =0; x < resolution; x++, i++)
+            {
+                CreateVoxel(i, x, y);
+            }
+        }
+        SetVoxelColors();
+    }
+
+    private void CreateVoxel (int i, int x, int y)
+    {
+        Vector3 voxelScale = Vector3.one * voxelSize * 0.9f;
+        GameObject instance = Instantiate(voxelPrefab) as GameObject;
+        instance.transform.parent = transform;
+        instance.transform.localPosition = new Vector3((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
+        instance.transform.localScale = voxelScale;
+        voxelMaterials[i] = instance.GetComponent<MeshRenderer>().material;
+    }
+
+    public void SetVoxel (int x, int y, bool state)
+    {
+        voxels[y * resolution + x] = state;
+        SetVoxelColors();
+    }
+
+    private void SetVoxelColors ()
+    {
+        for (int i = 0; i < voxels.Length; i++)
+        {
+            voxelMaterials[i].color = (voxels[i] ? Color.black : Color.white);
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/VoxelMap.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoxelMap : MonoBehaviour
+{
+    [SerializeField]
+    private float size = 2f;
+    [SerializeField]
+    private int voxelResolution = 8;
+    [SerializeField]
+    private int chunkResolution = 2;
+    [SerializeField]
+    private VoxelGrid VoxelGridPrefab;
+
+    private VoxelGrid[] chunks;
+    private float chunkSize, voxelSize, halfSize;
+    private float voxelSizeInverse, voxelResolutionInverse, chunkResolutionInverse;
+
+    private void Awake ()
+    {
+        InitializeCoefficients();
+
+        chunks = new VoxelGrid[chunkResolution * chunkResolution];
+        for (int i = 0, y = 0; y < chunkResolution; y++)
+        {
+            for (int x = 0; x < chunkResolution; x++, i++)
+            {
+                CreateChunk(i, x, y);
+            }
+        }
+
+        BoxCollider box = gameObject.AddComponent<BoxCollider>();
+        box.size = new Vector3(size, size);
+    }
+
+    private void InitializeCoefficients ()
+    {
+        voxelResolutionInverse = 1f / voxelResolution;
+        chunkResolutionInverse = 1f / chunkResolution;
+
+        halfSize = size * 0.5f;
+        chunkSize = size / chunkResolution;
+        voxelSize = chunkSize / voxelResolution;
+
+        voxelSizeInverse = 1f / voxelSize;
+    }
+
+    private void CreateChunk (int i, int x, int y)
+    {
+        VoxelGrid chunk = Instantiate(VoxelGridPrefab) as VoxelGrid;
+        chunk.Initialize(voxelResolution, chunkSize);
+        chunk.transform.parent = transform;
+        chunk.transform.localPosition = new Vector3(x * chunkSize - halfSize, y * chunkSize - halfSize);
+        chunks[i] = chunk;
+    }
+
+    private void Update ()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    EditVoxels(transform.InverseTransformPoint(hit.point));
+                }
+            }
+        }
+    }
+
+    private void EditVoxels (Vector3 point)
+    {
+        // TODO test float to int conversion vs int division
+
+        int voxelX = (int)((point.x + halfSize) * voxelSizeInverse);
+        int voxelY = (int)((point.y + halfSize) * voxelSizeInverse);
+        int chunkX = (int)(voxelX * voxelResolutionInverse);
+        int chunkY = (int)(voxelY * voxelResolutionInverse);
+        //Debug.Log(voxelX + ", " + voxelY + " in chunk " + chunkX + ", " + chunkY);
+
+        voxelX -= chunkX * voxelResolution;
+        voxelY -= chunkY * voxelResolution;
+        chunks[chunkY * chunkResolution + chunkX].SetVoxel(voxelX, voxelY, true);
+    }
+}
